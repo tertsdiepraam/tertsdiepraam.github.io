1425a38173ddf3a267148b263f4ac14a50c55e8a
Terts Diepraam <terts.diepraam@gmail.com>
2017-09-16 17:58:28 +0200

    New structure in FML.js

--- FML.js
+++ FML.js
@@ -4,41 +4,7 @@ const DEFAULT_CSS_STYLE = {
   "#": "id"
 }
 
-function split_with_empty(s, delimiter) {
-  let array = []
-  let index = s.indexOf(delimiter)
-  while (index >= 0) {
-    array.push(s.slice(0,index))
-    s = s.slice(index+1, s.length)
-    index = s.indexOf(delimiter)
-  }
-  array.push(s)
-  return array
-}
-
-function process_attribute(attr, style) {
-  type = style[attr[0]]
-  value = attr.slice(1, attr.length)
-  return [type, value]
-}
-
-function decodeHeader(line, style) {
-  const [name, ...expressions] = line.slice(1, line.length-1).trim().split()
-  const element = {
-    name: name.trim(),
-    content: ""
-  }
-  const processed_attributes = expressions.map(attr => process_attribute(attr, style))
-  for (let [key, value] of processed_attributes) {
-    if (e.hasOwnProperty(key)) {
-      element[key] += ' ' + value
-    } else {
-      element[key] = value
-    }
-  }
-  return element
-}
-
+// User called functions
 function decode(str, style) {
   let td_lines = split_with_empty(str, "\n")
   let elements = []
@@ -58,6 +24,36 @@ function decode(str, style) {
   return elements
 }
 
+function decodeToHTML(str, style) {
+  if (typeof style === 'undefined') { style = DEFAULT_CSS_STYLE }
+  let decoded = decode(str, style)
+  return decoded.map(elementToHTML)
+}
+
+// Decoding helper functions
+function decodeHeader(line, style) {
+  const [name, ...expressions] = line.slice(1, line.length-1).trim().split()
+  const element = {
+    name: name.trim(),
+    content: ""
+  }
+  const processed_attributes = expressions.map(attr => process_attribute(attr, style))
+  for (let [key, value] of processed_attributes) {
+    if (e.hasOwnProperty(key)) {
+      element[key] += ' ' + value
+    } else {
+      element[key] = value
+    }
+  }
+  return element
+}
+
+function process_attribute(attr, style) {
+  type = style[attr[0]]
+  value = attr.slice(1, attr.length)
+  return [type, value]
+}
+
 function elementToHTML(element) {
   let DOM_element = document.createElement(element.name)
   DOM_element.innerHTML = element.content
@@ -69,8 +65,15 @@ function elementToHTML(element) {
   return DOM_element
 }
 
-function decodeToHTML(str, style) {
-  if (typeof style === 'undefined') { style = DEFAULT_CSS_STYLE }
-  let decoded = decode(str, style)
-  return decoded.map(elementToHTML)
+// General helper functions
+function split_with_empty(s, delimiter) {
+  let array = []
+  let index = s.indexOf(delimiter)
+  while (index >= 0) {
+    array.push(s.slice(0,index))
+    s = s.slice(index+1, s.length)
+    index = s.indexOf(delimiter)
+  }
+  array.push(s)
+  return array
 }
